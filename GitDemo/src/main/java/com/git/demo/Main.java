31b336f7c0e6a22edb76ce1b566af7ff6d9bd04c
venkatkumar366 <49677825+venkatkumar366@users.noreply.github.com>
2022-07-21 10:44:06 +0530

    Adding 1st commit from fork

--- GitDemo/src/main/java/com/git/demo/Main.java
+++ GitDemo/src/main/java/com/git/demo/Main.java
@@ -9,6 +9,12 @@ public class Main {
 
     private static void demo() {
         System.out.println("Add");
+        int a =3,b =4;
+        add(a,b);
+    }
+
+    private static void add(int a, int b) {
+        System.out.println(a+b);
         System.out.println("//change List is nothing but stash - have added multiple changelist like DS JAVA8 Java 11");
     }
 }
